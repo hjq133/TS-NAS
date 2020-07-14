62acaceb380a2aabc39a1cfd5229879ea337767e
hjq133 <1399615054@qq.com>
2020-07-14 23:38:09 +0800

    add all thing except bandit search

--- cnn/model_search.py
+++ cnn/model_search.py
@@ -4,57 +4,6 @@ import torch.nn.functional as F
 from operations import OPS, FactorizedReduce, ReLUConvBN
 from genotypes import PRIMITIVES, Genotype, NAME2ID
 
-class MixedLayer(nn.Module):
-    """
-    a mixtures output of 8 type of units.
-
-    we use weights to aggregate these outputs while training.
-    and softmax to select the strongest edges while inference.
-    """
-
-    def __init__(self, c, stride):
-        """
-
-        :param c: 16
-        :param stride: 1
-        """
-        super(MixedLayer, self).__init__()
-
-        self.layers = nn.ModuleList()
-        """
-        PRIMITIVES = [
-                    'none',
-                    'max_pool_3x3',
-                    'avg_pool_3x3',
-                    'skip_connect',
-                    'sep_conv_3x3',
-                    'sep_conv_5x5',
-                    'dil_conv_3x3',
-                    'dil_conv_5x5'
-                ]
-        """
-        for primitive in PRIMITIVES:
-            # create corresponding layer
-            layer = OPS[primitive](c, stride, False)
-            # append batchnorm after pool layer
-            if 'pool' in primitive:
-                # disable affine w/b for batchnorm
-                layer = nn.Sequential(layer, nn.BatchNorm2d(c, affine=False))
-
-            self.layers.append(layer)
-
-    def forward(self, x, weights):
-        """
-
-        :param x: data
-        :param weights: alpha,[op_num:8], the output = sum of alpha * op(x)
-        :return:
-        """
-        res = [w * layer(x) for w, layer in zip(weights, self.layers)]
-        # element-wise add by torch.add
-        res = sum(res)
-        return res
-
 
 class Cell(nn.Module):
 
@@ -90,12 +39,9 @@ class Cell(nn.Module):
         # steps inside a cell
         self.steps = steps  # 4
         self.multiplier = multiplier  # 4
-
         self.layers = nn.ModuleList()
 
         for i in range(self.steps):
-            # for each i inside cell, it connects with all previous output
-            # plus previous two cells' output
             for j in range(2 + i):
                 # for reduction cell, it will reduce the heading 2 inputs only
                 stride = 2 if reduction and j < 2 else 1
@@ -103,18 +49,13 @@ class Cell(nn.Module):
 
     def forward(self, s0, s1, genotype):
         """
-
         :param s0:
         :param s1:
         :param genotype: [14, 8]
         :return:
         """
-        # print('s0:', s0.shape,end='=>')
         s0 = self.preprocess0(s0)  # [40, 48, 32, 32], [40, 16, 32, 32]
-        # print(s0.shape, self.reduction_prev)
-        # print('s1:', s1.shape,end='=>')
         s1 = self.preprocess1(s1)  # [40, 48, 32, 32], [40, 16, 32, 32]
-        # print(s1.shape)
 
         states = [s0, s1]
         offset = 0
@@ -141,7 +82,7 @@ class Cell(nn.Module):
             states += [s]
             offset += len(states)
         # concat along dim=channel
-        return torch.cat([states[i] for i in self._concat], dim=1)  # 6 of [40, 16, 32, 32]
+        return torch.cat([states[i] for i in concat], dim=1)  # 6 of [40, 16, 32, 32]
 
 
 class Network(nn.Module):
@@ -197,48 +138,15 @@ class Network(nn.Module):
             cell = Cell(steps, multiplier, cpp, cp, c_curr, reduction, reduction_prev)
             # update reduction_prev
             reduction_prev = reduction
-
             self.cells += [cell]
-
             cpp, cp = cp, multiplier * c_curr
-
         # adaptive pooling output size to 1x1
         self.global_pooling = nn.AdaptiveAvgPool2d(1)
         # since cp records last cell's output channels
         # it indicates the input channel number
         self.classifier = nn.Linear(cp, num_classes)
 
-        # k is the total number of edges inside single cell, 14
-        k = sum(1 for i in range(self.steps) for j in range(2 + i))
-        num_ops = len(PRIMITIVES)  # 8
-
-        # TODO
-        # this kind of implementation will add alpha into self.parameters()
-        # it has num k of alpha parameters, and each alpha shape: [num_ops]
-        # it requires grad and can be converted to cpu/gpu automatically
-        self.alpha_normal = nn.Parameter(torch.randn(k, num_ops))
-        self.alpha_reduce = nn.Parameter(torch.randn(k, num_ops))
-        with torch.no_grad():
-            # initialize to smaller value
-            self.alpha_normal.mul_(1e-3)
-            self.alpha_reduce.mul_(1e-3)
-        self._arch_parameters = [
-            self.alpha_normal,
-            self.alpha_reduce,
-        ]
-
-    def new(self):
-        """
-        create a new model and initialize it with current alpha parameters.
-        However, its weights are left untouched.
-        :return:
-        """
-        model_new = Network(self.c, self.num_classes, self.layers, self.criterion).cuda()
-        for x, y in zip(model_new.arch_parameters(), self.arch_parameters()):
-            x.data.copy_(y.data)
-        return model_new
-
-    def forward(self, x):
+    def forward(self, x, genotype):
         """
         in: torch.Size([3, 3, 32, 32])
         stem: torch.Size([3, 48, 32, 32])
@@ -253,36 +161,23 @@ class Network(nn.Module):
         pool:   torch.Size([16, 256, 1, 1])
         linear: [b, 10]
         :param x:
+        :param genotype:
         :return:
         """
         # print('in:', x.shape)
         # s0 & s1 means the last cells' output
         s0 = s1 = self.stem(x)  # [b, 3, 32, 32] => [b, 48, 32, 32]
         # print('stem:', s0.shape)
-
         for i, cell in enumerate(self.cells):
-            # weights are shared across all reduction cell or normal cell
-            # according to current cell's type, it choose which architecture parameters
-            # to use
-            if cell.reduction:  # if current cell is reduction cell
-                weights = F.softmax(self.alpha_reduce, dim=-1)
-            else:
-                weights = F.softmax(self.alpha_normal, dim=-1)  # [14, 8]
-            # execute cell() firstly and then assign s0=s1, s1=result
-            s0, s1 = s1, cell(s0, s1, weights)  # [40, 64, 32, 32]
-            # print('cell:',i, s1.shape, cell.reduction, cell.reduction_prev)
-            # print('\n')
-
+            s0, s1 = s1, cell(s0, s1, genotype)  # [40, 64, 32, 32]
         # s1 is the last cell's output
         out = self.global_pooling(s1)
         # print('pool', out.shape)
         logits = self.classifier(out.view(out.size(0), -1))
-
         return logits
 
     def loss(self, x, target):
         """
-
         :param x:
         :param target:
         :return:
@@ -290,18 +185,13 @@ class Network(nn.Module):
         logits = self(x)
         return self.criterion(logits, target)
 
-    def arch_parameters(self):
-        return self._arch_parameters
-
     def genotype(self):
         """
-
         :return:
         """
 
         def _parse(weights):
             """
-
             :param weights: [14, 8]
             :return:
             """
